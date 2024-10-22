533d8b5f6c76842210e3220ecc5cc3b129317a56
cofcat456 <a25690756@gmail.com>
2024-10-22 12:54:41 +0800

    feat: add atomic

---
+++ src/atomic/TextInput.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const { label, type, inputClass, disabled, placeholder } = defineProps<{
+  label?: string
+  type?: 'input' | 'textarea'
+  inputClass?: string
+  disabled?: boolean
+  placeholder?: string
+}>()
+
+const modelValue = defineModel<string>('modelValue')
+</script>
+
+<template>
+  <label flex="~ col gap-1">
+    <span v-if="label" op75>{{ label }}</span>
+    <textarea
+      v-if="type === 'textarea'"
+      v-model="modelValue"
+      :placeholder="placeholder ?? label"
+      box-input
+      :class="inputClass"
+      :disabled
+    />
+    <input
+      v-else
+      v-model="modelValue"
+      :placeholder="placeholder ?? label"
+      box-input
+      :class="inputClass"
+      :disabled
+    >
+  </label>
+</template>
